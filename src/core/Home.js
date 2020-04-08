4fc2dc4ed4400a82be1c847c1532b2e74a5895a8
Jason Alway <jtalway@gmail.com>
2020-04-08 16:22:33 -0500

    changed homepage title and added github links

--- src/core/Home.js
+++ src/core/Home.js
@@ -5,8 +5,14 @@ const Home = () => (
   <div>
     <div className="jumbotron">
       <h2>Home</h2>
-      <p className="lead">Welcome to the Social Network app</p>
+      <p className="lead">Welcome to the Social Network web app</p>
       <p className="lead">... in development by Jason Alway... using MERN Stack</p>
+      <hr />
+      <h6>GitHub links</h6>
+        <p>
+        <a className="nav-link" href="https://github.com/jtalway/social-network-client">Client</a>
+        <a className="nav-link" href="https://github.com/jtalway/social-network-api">API</a>
+        </p>
     </div>
     <div className="container">
       <Posts />
